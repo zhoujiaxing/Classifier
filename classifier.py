03fee8d8b6810a96e4b34fd7b51a6937efc95c4a
Zhou Jiaxing <zjx@ip-172-31-31-196.ap-southeast-1.compute.internal>
2016-03-29 10:11:50 +0000

    Debug 2 on server

--- classifier.py
+++ classifier.py
@@ -4,7 +4,7 @@ import cPickle
 class Tool(object):
 	#cates=["Auto","Business","Cricket","doc","Education","Entertainment","Health","Lifestyle","National","Politics","Sports","Technology","Top Stories","World"]
 	def __init__(self,category="Auto"):
-		self.cates=["Auto","Business","Cricket","doc","Education","Entertainment","Health","Lifestyle","National","Politics","Sports","Technology","Top Stories","World"]
+		self.cates=["Auto","Business","Cricket","Education","Entertainment","Health","Lifestyle","National","Politics","Sports","Technology","Top Stories","World"]
 		self.category = category
 		self.category_anther = self.getanther()
 	def getanther(self):
@@ -29,53 +29,43 @@ class Classifier(object):
 		self.category = category
 		self.tool = Tool(category)
 		self.category_anther = self.tool.category_anther
-	def getrain(self):
+	def trainclassifier(self):
 		client = mongocli.MongoCli()
-		datas = client.getdata('hinews','article',1000)
+		datas = client.getdata('hinews','article',80000)
 		#add positive elements 120
+		count = 1
 		for data in datas:
-			tfidf_vec = cPickle.loads(data['feature'])
+			tfidf_vec = cPickle.loads(data['feature']).toarray()[0]
 			categorys = data['category']
-			count = 1
 			if self.tool.categoryisok(categorys):
 				self.train_X.append(tfidf_vec)
 				self.train_Y.append(1)
 				count = count + 1
 			if count > 120:
+				print ("%s is over....")%self.category
 				break
+		print count
 		#add negative elements 120 divide into 12 part
 		for category in self.category_anther:
+			count = 1
 			for data in datas:
-				tfidf_vec = cPickle.loads(data['feature'])
+				tfidf_vec = cPickle.loads(data['feature']).toarray()[0]
 				categorys = data['category']
-				count = 1
 				if self.tool.categoryisoks(category,categorys):
 					self.train_X.append(tfidf_vec)
 					self.train_Y.append(0)
+					count = count + 1
 				if count > 10:
+					print("%s is over.....")%category
 					break
-		print "train is over..."
-
-		'''
-		self.train_X.append([1,0,1,0,0,0,0,0,0,0])
-		self.train_Y.append(1)
-		self.train_X.append([1,0,0,0,0,0,0,0,0,1])
-		self.train_Y.append(1)
-		self.train_X.append([1,0,0,1,1,0,0,0,1,1])
-		self.train_Y.append(1)
-		self.train_X.append([0,0,0,0,0,1,1,1,1,1])
-		self.train_Y.append(0)
-		self.train_X.append([0,0,0,0,0,0,1,1,1,1])
-		self.train_Y.append(0)
-		'''
-	def trainClassifier(self):
+			print count
 		self.classifier.fit(self.train_X,self.train_Y)
+		print "train is over...."
 	def getarget(self,text):
 		return self.classifier.predict(text)
 
 if __name__ == "__main__":
 	cf=Classifier()
-	cf.getrain()
-	#cf.trainClassifier()
+	cf.trainclassifier()
 	#target=cf.getarget([0,0,0,0,0,0,0,0,1,0])
 	print "game over"
